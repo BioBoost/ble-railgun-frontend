9441232661cabc7d27d57507018b841015a24e4e
Nico De Witte <nico.dewitte@outlook.com>
2019-10-03 20:54:04 +0200

    Allow railgun to shoot

--- railgun.js
+++ railgun.js
@@ -4,7 +4,12 @@ let connect = () => {
   log("Connecting to railgun ...");
   navigator.bluetooth.requestDevice({
     filters: [
-      {services: ['battery_service']},
+      {services: [
+          // All accessible services need to be added
+          'battery_service',
+          'eac9cf2d-1d6e-4ab5-8582-bdc124b15e52'
+        ]
+      },
       { name: 'railgun' }
     ]
   })
@@ -26,7 +31,31 @@ let shoot = () => {
 }
 
 let reload = () => {
-  log("Reloading");
+  if (device) {
+    log("Reloading railgun ...");
+    log("Getting RailgunCommand Service ...");
+    device.gatt.getPrimaryService('eac9cf2d-1d6e-4ab5-8582-bdc124b15e52')
+    .then(service => {
+      log('Getting Charge Characteristic ...');
+      return service.getCharacteristic('b65a60ce-b0e9-43a3-a991-4a908a5705bc');
+    })
+    .then(characteristic => {
+      log('Writing Charge Characteristic ...');
+
+      // Writing 1 is the signal to reset energy expended.
+      let chargePercentage = Uint8Array.of(50);
+      return characteristic.writeValue(chargePercentage);
+    })
+    .then(_ => {
+      log('Railgun successfully recharged');
+    })
+    .catch(error => {
+      log('ERROR ' + error);
+    });
+
+  } else {
+    log("Not connected to a railgun");
+  }
 }
 
 let disconnect = () => {
