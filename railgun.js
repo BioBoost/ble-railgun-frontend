4b53389b0f3db81d85df3eda1c05691763d14413
Nico De Witte <nico.dewitte@outlook.com>
2019-10-03 19:37:55 +0200

    Implement connect and disconnect methods

--- railgun.js
+++ railgun.js
@@ -1,5 +1,24 @@
+let device = undefined;
+
 let connect = () => {
-  log("Connecting");
+  log("Connecting to railgun ...");
+  navigator.bluetooth.requestDevice({
+    filters: [
+      {services: ['battery_service']},
+      { name: 'railgun' }
+    ]
+  })
+  .then(dev => {
+    device = dev;
+    log('Connecting to GATT Server ...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    log('Connected to GATT server');
+  })
+  .catch(error => {
+    log('ERROR ' + error);
+  });
 }
 
 let shoot = () => {
@@ -10,8 +29,20 @@ let reload = () => {
   log("Reloading");
 }
 
+let disconnect = () => {
+  if (device) {
+    log("Disconnecting ...");
+    device.gatt.disconnect();
+    device = undefined;
+    log("Successfully disconnected from railgun");
+  } else {
+    log("Not connected to a railgun");
+  }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
   document.getElementById("connect").addEventListener("click", connect);
   document.getElementById("shoot").addEventListener("click", shoot);
   document.getElementById("reload").addEventListener("click", reload);
+  document.getElementById("disconnect").addEventListener("click", disconnect);
 });
